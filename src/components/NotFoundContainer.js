8519e19cecb4874486fcd56484bd51c395dab0d7
Alexandre Alves <alexandredias1995@gmail.com>
2018-11-24 20:49:33 -0200

    Update NotFoundPage

--- src/components/NotFoundContainer.js
+++ src/components/NotFoundContainer.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import { Button, Jumbotron } from "react-bootstrap";
+import { withRouter } from 'react-router-dom';
 
 class NotFoundContainer extends Component {
+
+    goToLogin() {
+        this.props.history.push('/login');
+     }
+
     render() {
         return (
-            <Jumbotron>
-                <h1>Hello, world!</h1>
+            <Jumbotron style={{ margin: 30, padding: 150, backgroundColor: "rgb(255, 174, 168)" }}>
+                <h1>404</h1>
+                <h2>Ooops! Parece que você achou uma página que não existe</h2>
                 <p>
-                    This is a simple hero unit, a simple jumbotron-style component for calling
-                    extra attention to featured content or information.
+                    Se eu fosse você voltaria para o login. Vamos fingir que nada disso aconteceu, ok?
             </p>
                 <p>
-                    <Button bsStyle="primary">Learn more</Button>
+                    <Button bsStyle="primary" onClick={() => this.goToLogin()}>Ir para Login</Button>
                 </p>
             </Jumbotron>
         )
     }
 }
 
-export default NotFoundContainer;
+export default withRouter(NotFoundContainer);
