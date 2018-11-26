bd0787b09e130024f8efa547e461d339031c60d0
Alexandre Alves <alexandredias1995@gmail.com>
2018-11-25 23:58:01 -0200

    Update document title

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { Provider } from "react-redux"
 import store from './store';
 import Routes from './routes';
 
+window.document.title = "B2C Anglo - Admin"
+
 render(
     <Provider store={store}><Routes /></Provider>
     , document.getElementById("root"));
