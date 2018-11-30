2fe1e7d7b78a46db1e17846ef6d98ef5cb326879
Alexandre Alves <alexandredias1995@gmail.com>
2018-11-30 19:29:31 -0200

    Fix params prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-// const API_URL = JSON.stringify('https://www.b2c-app.info/api')
-const API_URL = JSON.stringify('http://192.168.15.15:9000/')
+const API_URL = JSON.stringify('https://www.b2c-app.info/api')
 
 const { resolve } = require('path');
 
